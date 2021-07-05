ec16dada3d94b42750982fc0da3713c99ef5a509
albertoccelli <albertoccelli@gmail.com>
2021-07-05 17:54:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 
 from source.testloop import Test
 from source.testloop import splash
+from source.play import playWav
+from source.recorder import Recorder
+from threading import Thread
 
 splash()
 
 try:
     t = Test()
     input("\nPress ENTER TO CONTINUE")
+    t.recordTreshold(15)
     t.execution()
 
 except KeyboardInterrupt:
