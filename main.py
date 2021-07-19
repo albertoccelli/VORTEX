3985dbca3a163c612c690bf86083ccff1452a6d7
albertoccelli <albertoccelli@gmail.com>
2021-07-19 11:33:41 +0200

    Update

--- main.py
+++ main.py
@@ -17,28 +17,30 @@ clear_console()
 
 try:
     t = Test()
-    if not t.mCalibrated:
+    if not t.mCalibrated or not t.recorder.calibrated[t.micChannel] or not t.recorder.calibrated[t.earChannel]:
         MsgBox = tk.messagebox.askyesno('Calibration', 'Do you want to calibrate the microphones and the mouth?',
                                         icon='question')
-        if MsgBox:
-            # calibration of measurement microphone
-            if not t.recorder.calibrated[t.micChannel]:
-                clear_console()
-                print("Calibrating measurement microphone\n")
-                t.calibrate_mic()
-                input("\nPress ENTER to proceed...\n-->")
-            # calibration of Oscar's ear
-            if not t.recorder.calibrated[t.earChannel]:
-                clear_console()
-                print("Calibrating artificial ear\n")
-                t.calibrate_ear()
-                input("\nPress ENTER to proceed...\n-->")
-            # calibration of artificial mouth
-            clear_console()
-            print("------------------------------------------------------------------")
-            input("Calibrating mouth: please place the measurement microphone at the MRP and press ENTER\n-->")
-            t.calibrate_mouth()  # calibration of the mouth
-            input("\nPress ENTER to proceed...\n-->")
+    else:
+        MsgBox = not (tk.messagebox.askyesno('Calibration', 'Calibration files found. Do you want to proceed? '
+                                                            '(Press NO to redo the calibration.',
+                                             icon='question'))
+    if MsgBox:
+        # calibration of measurement microphone
+        clear_console()
+        print("Calibrating measurement microphone\n")
+        t.calibrate_mic()
+        input("\nPress ENTER to proceed...\n-->")
+        # calibration of Oscar's ear
+        clear_console()
+        print("Calibrating artificial ear\n")
+        t.calibrate_ear()
+        input("\nPress ENTER to proceed...\n-->")
+        # calibration of artificial mouth
+        clear_console()
+        print("------------------------------------------------------------------")
+        input("Calibrating mouth: please place the measurement microphone at the MRP and press ENTER\n-->")
+        t.calibrate_mouth()  # calibration of the mouth
+        input("\nPress ENTER to proceed...\n-->")
     t.execution()  # execute test
     if t.completed:
         messagebox.showinfo(t.testName, "Test completed!")
