14811b29fb2d385550bc53844e3aceab6ac1f6d1
albertoccelli <albertoccelli@gmail.com>
2021-08-02 11:15:53 +0200

    Update

--- phrases/harman/rename_wavs.py
+++ phrases/harman/rename_wavs.py
@@ -1,16 +1,16 @@
 import os
 
 cPath = os.getcwd()
-lang = "FRF"
+lang = "PTP"
 
 
 wavs = []
 files = os.listdir(lang)
 for i in files:
-    if i.split(".")[-1] == "wav":
+    if i.split(".")[-1] == "wav" and i[:3] == lang:
         wavs.append(i)
 
-os.chdir("FRF")
+os.chdir(lang)
 
 for j in range(len(wavs)):
-    os.rename(wavs[j], "%s_%03d.wav"%("FRF", j))
+    os.rename(wavs[j], "%s_%03d.wav"%(lang, j))
