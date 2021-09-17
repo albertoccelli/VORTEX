9fa7da42935afaeff02f4368aedbf05845e7fdb8
albertoccelli <albertoccelli@gmail.com>
2021-09-17 11:40:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from PyQt5 import QtGui
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+
 from libs.gui.about import Ui_About
 from libs.gui.newtest import Ui_Dialog
 from libs.gui.note import Ui_Note_Dialog
